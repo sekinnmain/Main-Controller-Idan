e7ad5ba9b89ddd33656324262093068de661a819
idantok <50590151+idantok@users.noreply.github.com>
2019-05-25 14:15:48 +0300

    dish orderservice and pay

---
+++ Dish.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace code_1
+{
+    class dish
+    {
+        public string DishName;
+        public float DishPrice;
+        public int DishSize;
+        public string DishDescripation;
+
+       public dish(string dishname,float dishprice,int dishSize,string dishDescripation)
+        {
+           DishName = dishname;
+            DishPrice = dishprice;
+            DishSize = dishSize;
+            DishDescripation = dishDescripation;
+        }
+        public string getDishName()
+        {
+            return DishName;
+        }
+        public float getPrice(String DishName, int DishSize)
+        {
+            return DishPrice;
+        }
+        public int getDishSize(String DishName)
+        {
+            return DishSize;  
+        }
+        public string getDishDescripation(String name)
+        {
+            return DishDescripation;
+        }
+    }
+}
